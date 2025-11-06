2a72437b3a2f8c1c36a4f2f740ac45d19d72ef6c
Не Р <ner8650@gmail.com>
2025-11-06 23:35:35 +0300

    147 task

---
+++ 147/test.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    void makeSorted(ListNode* head) {
+        ListNode* list = head;
+        while (list->next != nullptr && list->val > list->next->val) {
+            int buffer = list->val;
+            list->val = list->next->val;
+            list->next->val = buffer;
+            list = list->next;
+        }
+    }
+
+    ListNode* insertionSortList(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+        ListNode* sortList = new ListNode(head->val);
+        head = head->next;
+        while (head != nullptr) {
+            int curr_val = head->val;
+            ListNode* sortHead = head;
+            head = head->next;
+            sortHead->next = sortList;
+            makeSorted(sortHead);
+            sortList = sortHead;
+
+        }
+        return sortList;
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(3);
+    head->next->next = new ListNode(2);
+    head->next->next->next = new ListNode(4);
+    Solution class_;
+    ListNode* sort_head = class_.insertionSortList(head);
+    while (sort_head != nullptr) {
+        std::cout << sort_head->val << " -> ";
+        sort_head = sort_head->next;
+    }
+    return 0;
+}
